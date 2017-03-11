5f9c1344ca89bd91ee2f836339ee7a571c301ead
Yuriy Rogulya <relaxcoredev@gmail.com>
2017-03-11 04:05:14 +0200

    first commit

---
+++ TelegramBot/bot.rb
@@ -0,0 +1,13 @@
+require 'telegram/bot'
+token = '376993256:AAFpGwO_kYCzrYtZIssaw9fTzzWNihBJbWc'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(
+      chat_id:message.chat.id,
+      text:"Hello,#{message.from.first_name}")
+    end
+  end
+end
