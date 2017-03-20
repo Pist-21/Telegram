71c8ed91bc3a8e8cfe57726f4820dfab52c7fc62
relaxcore <relaxcoredev@gmail.com>
2017-03-20 03:42:32 +0200

    added teachers lessons+db

--- TelegramBot/bot.rb
+++ TelegramBot/bot.rb
@@ -18,13 +18,36 @@ Telegram::Bot::Client.run(Token) do |bot|
         text:Hello + "\n" + Availablecommands)
 
       when '/lessons'
-        group = "ПІст-21 ПІ-31 ПІ-32 ПІ-33 ПІ-34"
-        group_split = group.split(" ")
-        answer = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: group_split, one_time_keyboard: true)
-        bot.api.send_message(chat_id:message.chat.id,text:"Виберіть групу: ", reply_markup:answer)
+        type = "Студент Викладач"
+        type_split = type.split(" ")
+        answer = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: type_split, one_time_keyboard: true)
+        bot.api.send_message(chat_id:message.chat.id,text:"Виберіть тип користувача: ", reply_markup:answer)
+
+      when 'Студент'
+        student = 'ПІст-21 ПІ-31 ПІ-32 ПІ-33 ПІ-34'
+        student_split = student.split(" ")
+        @picked_studgroup = message.text
+        answer = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: student_split, one_time_keyboard: true)
+        bot.api.send_message(chat_id:message.chat.id,text:"Виберіть групу", reply_markup:answer)
+
+      when 'Викладач'
+        teacher = 'Журавчак Квятковський Крук Левченко Муляк Нитребич Федорчук Яковина'
+        teacher_split = teacher.split(" ")
+        @picked_teacher = message.text
+        answer = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: teacher_split, one_time_keyboard: true)
+        bot.api.send_message(chat_id:message.chat.id,text:"Виберіть викладача", reply_markup:answer)
+
+
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
       when 'ПІст-21', 'ПІ-31', 'ПІ-32', 'ПІ-33', 'ПІ-34'
+        @picked_group = message.text
+        period = "Сьогодні Тиждень День"
+        period_split = period.split(" ")
+        answer = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: period_split, one_time_keyboard: true)
+        bot.api.send_message(chat_id:message.chat.id,text:"Виберіть період: ", reply_markup:answer)
+
+      when 'Журавчак', 'Квятковський', 'Крук', 'Левченко', 'Муляк', 'Нитребич', 'Федорчук', 'Яковина'
         @picked_group = message.text
         period = "Сьогодні Тиждень День"
         period_split = period.split(" ")
