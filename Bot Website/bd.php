a1e92a3b9abd572e4f384f8e660d6c03ef6f4134
xelanik <xelanik@hotmail.com>
2017-04-12 00:33:21 +0300

    Edit and Fix registration

--- Bot Website/bd.php
+++ Bot Website/bd.php
@@ -1,4 +1,4 @@
 <?php
-        $db = mysql_connect ("сервер","логин","пароль");
-        mysql_select_db ("имя базы к которой подключаемся",$db);
+        $db = mysql_connect ("mysql.hostinger.com.ua","u829944273_xeli","1Fwqrlhfwqrlh");
+        mysql_select_db ("u829944273_pist",$db);
 ?> 
